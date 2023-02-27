ec4b39a01cdf9ed27d43a54af9cc6d0b927d61ca
tsachbak <126451282+tsachbak@users.noreply.github.com>
2023-02-27 10:09:12 +0200

    Add files via upload

--- test/src/HelloWorld.java
+++ test/src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world from Tsachbak");
+        System.out.println("is it changed?");
     }
 }
